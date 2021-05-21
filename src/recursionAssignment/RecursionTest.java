715ca5c629d7abe28dc54d96ae90a3826cfada52
lucymao-12 <lucy.mao21@ycdsbk12.ca>
2021-05-21 15:27:15 -0400

    realized I messed up red state: all three problems

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -21,21 +21,6 @@ public class RecursionTest{
         // test seven in front and back
         assertEquals(2, Recursion.count7(717));
     }
-    @Test
-    public void Test2(){
-        // no seven
-        assertEquals(0, Recursion.count7(0));
-    }
-    @Test
-    public void Test3(){
-        // all sevens
-        assertEquals(3, Recursion.count7(777));
-    }
-    @Test
-    public void Test4(){
-        // one seven
-        assertEquals(1, Recursion.count7(14217));
-    }
     /**
      * Tests for problem set 2
      */
@@ -43,21 +28,6 @@ public class RecursionTest{
     public void Test5(){
         // x at beginning and end
         assertEquals("hixx", Recursion.endX("xhxi"));
-    }
-    @Test
-    public void Test6(){
-        // only x
-        assertEquals("x", Recursion.endX("x"));
-    }
-    @Test
-    public void Test7(){
-        // no x
-        assertEquals("fdsdf", Recursion.endX("fdsdf"));
-    }
-    @Test
-    public void Test8(){
-        // one x in middle
-        assertEquals("fdsdfx", Recursion.endX("fdsdxf"));
     }
     /** 
      * Problem set 3 --> array
@@ -68,36 +38,7 @@ public class RecursionTest{
         int[] intNums = {1, 2, 20};
         assertEquals(true, Recursion.array220(intNums, 0));
     }
-    @Test
-    public void Test10(){
-        // no values that match
-        int[] intNums = {3, 6, 2};
-        assertEquals(false, Recursion.array220(intNums, 0));
-    }
-    @Test
-    public void Test11(){
-        // values at the beginning of the array
-        int[] intNums = {3, 30, 2};
-        assertEquals(true, Recursion.array220(intNums, 0));
-    }
-    @Test
-    public void Test12(){
-        // first value seperated from second: should return false
-        int[] intNums = {3, 2, 30};
-        assertEquals(false, Recursion.array220(intNums, 0));
-    }
-    @Test
-    public void Test13(){
-      // longer array
-      int[] intNums = {1, 4, 6, 3, 30, 3};
-        assertEquals(true, Recursion.array220(intNums, 0));
-    }
-    @Test
-    public void Test14(){
-      // only two values
-      int[] intNums = {1, 10};
-        assertEquals(true, Recursion.array220(intNums, 0));
-    }
+   
     
     
     @After
