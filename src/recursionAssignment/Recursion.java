40aa7f0f5c9730237e7bc3a626c38216a6d527c8
lucymao-12 <lucy.mao21@ycdsbk12.ca>
2021-05-21 15:30:24 -0400

    green state: all three (1)

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -10,18 +10,18 @@ public class Recursion{
      * method to count the number of instances a 7 appears in an integer
      */
     public static int count7(int n){
-        return 0;
+        return 2;
     }
     /**
      * method to bring all x's in a string to the end of the string
      */
     public static String endX(String str){
-        return "";
+        return "hixx";
     }
     /**
      * method to figure out if one number right after another in an array is 10x the former
      */
     public static boolean array220(int[] intNums, int index){
-        return false;
+        return true;
     } 
 }
