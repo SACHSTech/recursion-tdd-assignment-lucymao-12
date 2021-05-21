b6055b0b78d8d4f134ec2d3423fb9a35b9f07425
lucymao-12 <lucy.mao21@ycdsbk12.ca>
2021-05-21 15:32:14 -0400

    red state: all three (2)

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -21,6 +21,12 @@ public class RecursionTest{
         // test seven in front and back
         assertEquals(2, Recursion.count7(717));
     }
+    @Test
+    public void Test2(){
+        // no seven
+        assertEquals(0, Recursion.count7(0));
+    }
+    
     /**
      * Tests for problem set 2
      */
@@ -28,6 +34,11 @@ public class RecursionTest{
     public void Test5(){
         // x at beginning and end
         assertEquals("hixx", Recursion.endX("xhxi"));
+    }
+    @Test
+    public void Test6(){
+        // only x
+        assertEquals("x", Recursion.endX("x"));
     }
     /** 
      * Problem set 3 --> array
@@ -38,13 +49,15 @@ public class RecursionTest{
         int[] intNums = {1, 2, 20};
         assertEquals(true, Recursion.array220(intNums, 0));
     }
-   
-    
-    
+    @Test
+    public void Test10(){
+        // no values that match
+        int[] intNums = {3, 6, 2};
+        assertEquals(false, Recursion.array220(intNums, 0));
+    }
     @After
     public void afterTest(){
       // some steps to perform after tests are run
-    }
-    
+    }  
     
 }
